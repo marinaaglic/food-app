cc6882c470ce3ade3e3c0f76b4408071a90b13a8
marinaaglic <marinaaglic11@gmail.com>
2024-03-04 16:58:17 +0100

    styling added

---
+++ components/main-header/main-header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import logo from "../../assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./main-background-header";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logo} alt="A plate of food." priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
