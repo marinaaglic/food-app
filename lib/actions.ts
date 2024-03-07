a296c8ba254886d5b0de9bc18611312cbef5fdc7
marinaaglic <marinaaglic11@gmail.com>
2024-03-07 19:26:11 +0100

    form validation added

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,7 +4,14 @@ import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 import slugify from "slugify";
 
-export async function shareMeal(formData: FormData) {
+function isInvalidText(text: string) {
+  return !text || text.trim() === "";
+}
+
+export async function shareMeal(
+  state: { message: string } | null,
+  formData: FormData
+) {
   const title = formData.get("title") as string;
   const slug = slugify(title, { lower: true });
   const imagePath = `/images/${slug}.${(formData.get("image") as File).name
@@ -20,6 +27,21 @@ export async function shareMeal(formData: FormData) {
     creator: formData.get("name") as string,
     creator_email: formData.get("email") as string,
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    return {
+      message: "Invalid input.",
+    };
+  }
   await saveMeal(meal);
 
   redirect("/meals");
